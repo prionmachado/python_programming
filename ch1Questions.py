bfb86286748d3f89b1629f0be50976f819f8f26f
prionmachado <prion2587@gmail.com>
2025-08-28 22:15:06 +0530

    First Commit

---
+++ ch1Questions.py
@@ -0,0 +1,18 @@
+# calculate area of square
+side = int(input("Enter the side: "))
+print("Area of square =", side*side)  
+
+
+#input 2 floating numbers and print average
+a = float(input("Enter the first number:"))
+b = float(input("Enter the second number:"))
+print("The average is = ",(a+b)/2)  
+
+
+#input 2 int numbers(a,b) , print true if a is greater or equal to b,if not print false
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+if(a>=b):
+    print("True")
+else:
+    print("False")   
---
+++ operators.py
@@ -0,0 +1,45 @@
+#types of operators
+
+#arithmetic operators
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: ")) 
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b) 
+
+#Relational operators
+a = 9
+b = 5
+
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a > b)
+print(a < b) 
+
+#Assignment operators
+a,b,c = 10,10,10
+d,e,f = 10,10,10
+b+=10
+c-=10
+d*=10
+e/=10
+e%=10
+f**=10
+print(b)
+print(c) 
+print(d)
+print(e)
+print(f)  
+
+#logical operators
+print(not False)
+print(not True)
+print(True and True)
+print(False and False)
+print(True or True)
+print(False or False) 
