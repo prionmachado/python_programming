c321885126689e1dbac1fe94f17b98de84cf5ae2
prionmachado <prion2587@gmail.com>
2025-08-29 16:19:24 +0530

    Add new folder

---
+++ Tuple/tuple.py
@@ -0,0 +1,11 @@
+tup = (1,2,3)
+
+print(len(tup))
+print(tup[0])
+print(tup[1])
+print(type(tup))
+
+tup1 = (1,)  #write comma , if you dont write comma python will treat it as integer
+print(tup1)
+
+
---
+++ Tuple/tupleslicing.py
@@ -0,0 +1,6 @@
+tup = (1,2,3,4)
+
+print(tup[0:3])
+print(tup[1:4])
+print(tup[-4:])
+print(tup[-3:-1]) 
